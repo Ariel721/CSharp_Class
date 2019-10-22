092c96a9bfd58c75ca4f7b8b5d15681075949816
ariel721 <ariel.r.garcia.c@gmail.com>
2019-10-22 01:06:39 -0700

    Assignment #4 program.cs

---
+++ Assignment04/Assignment04/Program.cs
@@ -0,0 +1,31 @@
+﻿//This program calculates the factorial of a small integer 
+//entered by the user
+
+using System;
+
+
+namespace Assignment04
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the factorial calculator for small integers"); //Greet User
+            Console.Write("Please enter a small integer:"); //Ask user to enter integer
+            string FactIndexSt =Console.ReadLine(); //assign read value to string
+            int FactIndexInt = int.Parse(FactIndexSt); //explicitly convert string type to int type
+            int Fact=FactIndexInt-1; //initialize variables 
+
+            while (Fact>0) //loop
+            {
+                FactIndexInt = FactIndexInt * Fact; //calculate
+                Fact = --Fact; //set index to -1
+
+            }
+
+            Console.WriteLine("The factorial of {0} is: {1}", FactIndexSt, FactIndexInt);
+            Console.ReadLine();
+
+        }
+    }
+}
